94fe2d1ea8d556ef8c88ad81170d7e9131fbf52b
massimo.daros <massimo.daros@external.cerved.com>
2021-04-12 18:24:15 +0200

    MyBaseCreated Controller trait to defiune common methods

--- customer-service/app/controllers/CustomerController.scala
+++ customer-service/app/controllers/CustomerController.scala
@@ -17,19 +17,18 @@ import scala.concurrent.{ ExecutionContext, Future }
 import scala.concurrent.duration._
 
 @Singleton
-class CustomerController @Inject () ( val controllerComponents: ControllerComponents, val system: ActorSystem ) ( implicit ec: ExecutionContext ) extends BaseController {
+class CustomerController @Inject () ( val controllerComponents: ControllerComponents, val system: ActorSystem ) ( implicit ec: ExecutionContext ) extends MyBaseController {
 
-  implicit val timeout: Timeout = 5.seconds
-  implicit val customerReads = Json.reads [Customer]
-  implicit val customerWrites = Json.writes [Customer]
-  implicit val apiErrorWrites = Json.writes [ApiError]
+  private implicit val timeout: Timeout = 5.seconds
+  private implicit val customerReads: Reads [Customer ] = Json.reads [Customer]
+  private implicit val customerWrites: OWrites [Customer ] = Json.writes [Customer]
 
   private val logger = Logger ( getClass )
 
   private val customerRepositoryActor = system.actorOf ( Props [CustomerRepositoryActor] )
 
 
-  def getCustomers () = Action.async { implicit request =>
+  def getCustomers (): Action [AnyContent ] = Action.async { implicit request =>
 
     logger.info ( "getCustomers ()" )
 
@@ -37,11 +36,11 @@ class CustomerController @Inject () ( val controllerComponents: ControllerCompon
 
       case CustomersRetrieved ( customers ) => Ok ( Json.toJson ( customers ) )
 
-      case message => InternalServerError ( Json.toJson ( ApiError ( 500, s"Unable to retrieve customers" ) ) )
+      case message => InternalServerError ( jsonApiError ( 500, s"Unable to retrieve customers" ) )
     }
   }
 
-  def createCustomer () = Action.async { implicit request =>
+  def createCustomer (): Action [AnyContent ] = Action.async { implicit request =>
 
     request.body.asJson.map { body =>
 
@@ -55,20 +54,20 @@ class CustomerController @Inject () ( val controllerComponents: ControllerCompon
 
             case CustomerAdded ( savedCustomer ) => Ok ( Json.toJson ( savedCustomer ) )
 
-            case message => InternalServerError ( Json.toJson ( ApiError ( 500, s"Unable to create customer $customer" ) ) )
+            case message => InternalServerError ( jsonApiError ( 500, s"Unable to create customer $customer" ) )
           }
         }
 
         case e @ JsError ( _ )  => {
 
           logger.error ( s"Unable to create customer $request.body.asJson" )
-          Future { BadRequest ( Json.toJson ( ApiError ( 400, s"Invalid body" ) ) ) }
+          Future { BadRequest ( jsonApiError ( 400, s"Invalid body" ) ) }
         }
       }
-    }.getOrElse ( Future { InternalServerError ( Json.toJson ( ApiError ( 500, s"Unable to create customer $request.body.asJson" ) ) ) } )
+    }.getOrElse ( Future { InternalServerError ( jsonApiError ( 500, s"Unable to create customer $request.body.asJson" ) ) } )
   }
 
-  def updateCustomer ( id: Int ) = Action.async { implicit request =>
+  def updateCustomer ( id: Int ): Action [AnyContent ] = Action.async { implicit request =>
 
     request.body.asJson.map { body =>
 
@@ -82,11 +81,11 @@ class CustomerController @Inject () ( val controllerComponents: ControllerCompon
 
             case CustomerUpdated ( updatedCustomer ) => Ok ( Json.toJson ( updatedCustomer ) )
 
-            case CustomerNotFound ( customerId ) => NotFound ( Json.toJson ( ApiError ( 404, s"Customer customerId $customerId not found" ) ) )
+            case CustomerNotFound ( customerId ) => NotFound ( jsonApiError ( 404, s"Customer customerId $customerId not found" ) )
 
             case message => {
 
-              InternalServerError ( Json.toJson ( ApiError ( 500, s"Unable to update customer, customerId $id" ) ) )
+              InternalServerError ( jsonApiError ( 500, s"Unable to update customer, customerId $id" ) )
             }
           }
         }
@@ -94,17 +93,17 @@ class CustomerController @Inject () ( val controllerComponents: ControllerCompon
         case e @ JsError ( _ )  => {
 
           logger.error ( s"Unable to update customer $id ERRORS: ${e.errors}" )
-          Future { BadRequest ( Json.toJson ( ApiError ( 400, s"Invalid body" ) ) ) }
+          Future { BadRequest ( jsonApiError ( 400, s"Invalid body" ) ) }
         }
       }
     }.getOrElse ( {
 
       logger.error ( s"Bad request. Unable to update customer $id" )
-      Future { BadRequest ( Json.toJson ( ApiError ( 400, s"Unable to update customer $id" ) ) ) }
+      Future { BadRequest ( jsonApiError ( 400, s"Unable to update customer $id" ) ) }
     } )
   }
 
-  def deleteCustomer ( customerId: Int ) = Action.async { implicit request =>
+  def deleteCustomer ( customerId: Int ): Action [AnyContent ] = Action.async { implicit request =>
 
     logger.info ( s"deleteCustomer ( $customerId )" )
 
@@ -112,7 +111,7 @@ class CustomerController @Inject () ( val controllerComponents: ControllerCompon
 
       case CustomerDeleted ( customerId ) => Ok ( Json.toJson ( customerId ) )
 
-      case message => InternalServerError ( Json.toJson ( ApiError ( 500, s"Unable to delete customer $customerId" ) ) )
+      case message => InternalServerError ( jsonApiError ( 500, s"Unable to delete customer $customerId" ) )
     }
   }
 }
